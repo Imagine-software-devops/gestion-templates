c28982d4d361704eec23c7177cd59e3b1983eb9b
hugo-cerezo <55873067+hugo-cerezo@users.noreply.github.com>
2023-07-29 22:09:44 +0200

    day 2 commit
    formatage propre du dossier de travail
    debut fonction main.py
    docker dont work

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import json
 import os
+from aws_setup import *
+
+def install_instance(machine,path,):
+        if machine == "PC":
+                pathInstall = path
+                os.system("cd "+pathInstall)
+        else:
+                create_ec2_instance()
+                key_path = 'json data'
+                instance_id = 'json data'
+                ssh_client = wait_for_ssh(instance_id, key_path)
+                execute_commands(ssh_client, commands_to_execute)#commands to execute == json data
 
 
-with open('./gestion-templates/exemple.json') as f:
-        # data = json.load('f')
-        data = f.read()
-        # print(f.read())
-        print(data)
+if __name__ == '__main__':
+
+                
+
         
